b951b4f9d918fea6a60ef73baa10ee34a7300a66
Neko <68886053+li7355608@users.noreply.github.com>
2025-09-01 15:10:27 +0800

    refactor: 删除 Main.java 中的冗余注释
    删除了 Main.java 文件中不再需要的注释，该注释提到了运行代码的方法。由于这个提示对于当前的代码实现来说是多余的，因此移除了这些行，提高了代码的可读性。

--- src/main/java/com/waveraven/Main.java
+++ src/main/java/com/waveraven/Main.java
@@ -2,8 +2,6 @@ package com.waveraven;
 
 import com.waveraven.moveout.calculator.BillingService;
 
-//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
-// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
 public class Main {
     public static void main(String[] args) {
         // 创建费用计算服务
